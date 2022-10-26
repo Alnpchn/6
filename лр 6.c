83f452c462b4727f684a81e87b0fefe5651ad1ff
Алина <112811472+Alnpchn@users.noreply.github.com>
2022-10-26 17:44:42 +0300

    Add files via upload

---
+++ лр 6.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include<stdio.h>
+#include <locale.h>
+#define P 3.14
+void main()
+{
+	int hour;
+	int min;
+	int sec;
+	float s;
+	setlocale(LC_ALL, "RUS");
+	puts("задание 1\n");
+	int years;
+	printf("int year:");
+	scanf("%d", &years);
+	if (years % 4 == 0)
+		printf("%d - високосный год\n", years);
+	else printf("%d - невисокосный год\n", years);
+
+	puts("задание 2\n");
+
+	float x, F;
+	/*
+	при x = 0,5 F(x) = -0,25
+	при x = 1,5 F(x) = 1,55
+	при x = -1,5 F(x) = 1,55
+	*/
+	printf("input X:");
+	scanf("%f", &x);
+	F = ((1 >= x) && (x >= 0)) ? (pow(x, 2) - x) : (pow(x, 2) - sin(P * pow(x, 2)));
+	printf("F(x)=%.2f", F);
+
+	
+
+
+
+
+}
