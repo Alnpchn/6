9e280c4b8555e34eb618fc851d692a8b89948db1
Алина <112811472+Alnpchn@users.noreply.github.com>
2022-11-08 20:49:36 +0300

    Update лр сам 6.c

--- лр сам 6.c
+++ лр сам 6.c
@@ -1,27 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
-int main()
+float main()
 {
 	setlocale(LC_ALL, "RUS");
-	int n = 0;
-	int sum = 0;
-	while (1) {
-		printf("Введите N<1000:\n");
-		scanf("%u", &n);
-		if (n > 1000) {
-			printf("Ошибка N>1000:\n");
-			return 0;
-		}
-		while (n != 0) {
-			sum += n % 10;
-			n /= 10;
-		}
-		if (sum % 3) {
-			printf("Сумма чисел не кратна 3\n");
-		}
-		else {
-			printf("Сумма чисел кратна 3\n");
-		}
+	int x, y, n, m, z;
+	puts("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ <1000");
+	scanf("%f", &x);
+	y = x / 10;
+	z = x / 100;
+	n = x % 10;
+	m = y + z + n;
+	if (m % 3 == 0) {
+		printf("Р§РёСЃР»Рѕ РєСЂР°С‚РЅРѕ 3");
 	}
-	return 0;
+	else
+		printf("Р§РёСЃР»Рѕ РЅРµ РєСЂР°С‚РЅРѕ 3");
 }
